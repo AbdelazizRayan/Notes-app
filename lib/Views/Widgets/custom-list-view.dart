c9198b1052ecd3e50334b4cded0a839df1119675
AbdelazizRayan <Abdelazizabodyrayan@outlook.com>
2024-01-12 14:22:59 +0400

    fetch data

--- lib/Views/Widgets/custom-list-view.dart
+++ lib/Views/Widgets/custom-list-view.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes/Views/Widgets/custom-note-card.dart';
+import 'package:notes/cubits/Add_note_cubit/notes/notes_cubit.dart';
+import 'package:notes/models/note_model.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-          padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.only(bottom: 4),
-              child: CustomNoteCard(),
-            );
-          }),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes =
+            BlocProvider.of<NotesCubit>(context).notes ?? [];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+              itemCount: notes.length,
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.only(bottom: 4),
+                  child: CustomNoteCard(),
+                );
+              }),
+        );
+      },
     );
   }
 }
